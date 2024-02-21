21c97715c06a4876165f8df5e135bd69d70a8f34
Sajdah Mustafa <132957619+sajilawaji@users.noreply.github.com>
2024-02-21 00:09:29 -0500

    done

---
+++ index.js
@@ -0,0 +1,220 @@
+//global variables are best at the top
+
+//Global Variables
+//Functions
+
+//Naming Conventions
+
+//Global variables this is better
+
+const var1 = 10;
+const var2 = 20;
+const var3 = "Hello World";
+
+// its developers choice to capitilze or not
+const VAR1 = 10;
+const VAR2 = 20;
+const VAR3 = "Hello World";
+
+//Functions
+
+const x = () => {};
+
+const z = () => {};
+
+const u = () => {};
+
+//Call function last
+
+x();
+
+// function functionName(){
+//
+// }
+//
+// const otherFunction = () => {
+//
+//
+// };
+//
+// // const learners = [ "John", "Jack", "Jill", "James"];
+//
+//
+// lets try this again
+//
+// const learners = [
+//
+// {
+// name: "something",
+// age: "something",
+// score: "76",
+//
+// },
+//
+// ];
+//
+// const assignments
+// = [{}, {}];
+//
+// This function should take in two arrays
+//
+// // function buildLearnerList(learners, assignments) {}
+//
+// buildStudent();
+//
+
+// function sum(numOne, numTwo) {
+//   if (numOne && numTwo) {
+// return numOne + numTwo;
+//   } else console.log("Missing Args");
+// }
+//
+// console.log(sum());
+//
+//Example again
+
+// let x = sum();
+// console.log(sum(10, 20));
+//
+
+// let x = 10;
+// const arr = [1, 2, 3, 4, 5];
+//
+// arr[2] = 20;
+//
+//
+// const arr = [1, 2, 3, 4, 5];
+//
+// arr[0] = "Hello World";
+//
+//
+// console.log(arr);
+
+//this function MUST HAVE TWO THINGS PASSED IN
+
+//function functionName( thingOne, thingTwo){}
+
+//functionName...not continued -- question
+
+//example of global variables at the top
+// let x = 10;
+//
+// console.log(x);
+
+// let y = 'Hello World'
+//
+// let someOtherThing = "Some new thing";
+//
+
+//General Notes
+//pretend var doesnt exist - use let and const - global variables
+//theres no practical reason for const
+
+// old variable example
+
+//function add(numOne, numTwo) {
+
+//return numOne + numTwo;
+
+//}
+
+//let result = add(20,30);
+
+//console.log(result);
+
+// let x = 10;
+// let y = 10;
+// let z = x + y;
+// console.log(z);
+//
+// function functionName() {
+//   console.log("hello world");
+//
+
+// }
+//
+// functionName();
+//
+//
+
+//
+// function functionName() {
+// console.log ("heallo world")
+// }
+//
+//
+
+//1: Blocking off code to be ran later
+//2: Returns
+//3: Input data
+
+// function returnTen () {
+// console.log("Hello");
+// console.log("World");
+//
+// }
+//
+//
+//
+// let x = returnTen();
+//
+// console.log(x);
+//
+
+//
+// return 10;
+//
+// }
+//
+// let x = returnTen()
+//
+//
+// console.log(x)
+//
+
+//1: Blocking off code to be run later
+//2: Returns data
+//3: Input data
+
+//two args required
+
+//right here -
+
+//
+// function returnTen(numOne, numTwo) {
+//
+// console.log(numOne);
+// console.log(numTwo);
+//
+// }
+//
+// two args required
+//
+// addEventListener(20, 30)
+//
+
+// let result = numOne + numTwo;
+
+// console.log(numOne)
+
+// console.log(result);
+
+// }
+
+// let x = 10;
+// let y = 5;
+// return x + y;
+
+//
+// addAndPrint();
+//
+//
+// two args required
+// addAndPrint (20, 30);
+//
+
+//
+// let returnedData = returnTen();
+//
+// console.log(returnedData)
+//
